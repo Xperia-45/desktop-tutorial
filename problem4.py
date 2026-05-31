35b63d80bd490dfdd2748b91cf87f5617445b051
Ayush <ayushkatoch2008@gmail.com>
2026-05-31 19:32:14 +0530

    Add files via upload

---
+++ problem4.py
@@ -0,0 +1,14 @@
+
+while True:
+    commmand = input("enter a command (start/stop/quit): ")
+    if commmand == "start":
+     print('car has started')
+    command = input("enter  a command (start/stop/quit): ")
+    if command == "stop":
+        print("car has stopped")
+    elif command == "quit":
+        print("quiting the game")
+        break
+    else:
+        print("invalid command")
+        
