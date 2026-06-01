cf46f4d3fb8a7f8dc99e8958c405802ca78cb929
Ayush <ayushkatoch2008@gmail.com>
2026-06-01 21:50:04 +0530

    Add files via upload

---
+++ problem 5.py
@@ -0,0 +1,26 @@
+
+bill=input("enterr the bill amount")
+member=input("arre you member of the store (yes/no)?")
+discount=""
+if member=="yes" and int(bill)>5000:
+    print("you get 20% discount")
+    discount=20
+elif member=="no" and int(bill)>5000:
+    print("you get 10% discount")
+    discount=10
+elif member=="yes" and int(bill)<5000 and int(bill)>2000:
+    print("you get 10% discount")
+    discount=10
+elif member=="no" and int(bill)<5000 and int(bill)>2000:
+    print("you get 5% discount")
+    discount=5
+else:
+    int(bill)<2000
+    print("you get no discount")
+    discount=0
+discount_amount=int(bill)*int(discount)/100
+amount_to_pay=int(bill)-discount_amount
+print("discount amount" + str(discount_amount))
+print("total amount to pay" + str(amount_to_pay))
+
+    
