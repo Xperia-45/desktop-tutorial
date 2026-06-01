16a944dfdc20e4dde82b81003d41f7718c3cbfe7
Ayush <ayushkatoch2008@gmail.com>
2026-06-01 21:48:13 +0530

    Refactor password.py for clarity and organization

--- password.py
+++ password.py
@@ -1,7 +1,9 @@
+#variables
 correct_username = "ayush"
 correct_password = "tasowe125933"
 username = input('enter your username:')
 password = input("enter your password: ")
+#statements
 if username == correct_username and password == correct_password:
     print("login successful")
 elif username == correct_username and password != correct_password:
