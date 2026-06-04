95d134902b6758e0595c7606131962b29c9d1be9
Ayush <ayushkatoch2008@gmail.com>
2026-06-04 14:02:04 +0530

    Add files via upload

--- marks.py
+++ marks.py
@@ -1,31 +1,31 @@
-#for student marksheet
-student=[1,2,3,4,5]
-for student_count in student:
- m=input("print marks obtained in maths")
- s=input("print marks obtained in science")
- e=input("print marks obtained in english")
- #for finding total
-total=int(m)+int(s)+int(e)
-avg=total/3
-if avg>=90:#grading
+students=[1,2]
+for student in students:
+  m=int(input("enter marks obtained in maths: "))
+  s=int(input("enter marks obtained in science: "))
+  e=int(input("enter marks obtsined in english: "))
+  total=int(m)+int(s)+int(e)
+  avg=total/3
+  if avg>=90:
     grade="A"
-elif avg<=89 and avg>=75:
+  elif avg<=89 and avg>=75:
     grade="B"
-elif avg<=74 and avg>=60:
+  elif avg<=74 and avg>=60:
     grade="C"
-elif avg<=59 and avg>=40:
+  elif avg<=59 and avg>=40:
     grade="D"
-else:
+  else:
     grade="F"
-#output statements
-print("===== EXAMINATION RESULT =====")
-print("student 1 : {}")
-print("marks obtained in maths : {}".format(m))
-print("marks obtained in science : {}".format(s))
-print("marks obtained in english : {}".format(e))
-print("total marks obtained : {}".format(total))
-print("average marks obtained : {}".format(avg))    
-print("grade obtained : {}".format(grade))
-print("================================")
-print("avarage of student {} from 
-#statemments left avrage comparing students for all code 
+    print("=======EXAMINATION RESULT========")
+    print("STUDENT NUMBER: ",student)
+    print("marks obtained in maths: ",m)
+    print("marks obtained in science: ",s)
+    print("marks obtained in english: ",e)
+    print("total marks obtained: ",total)
+    print("average marks obtained: ",avg)
+    print("grade obtained: ",grade)
+    print("=================================")
+averages=[]
+averages.append(avg)
+print(max(averages))
+print(min(averages))
+
