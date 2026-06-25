e8b77bb83d87de0cab5850ddf163ff4482c3cc95
Ayush <ayushkatoch2008@gmail.com>
2026-06-25 10:07:58 +0530

    Refactor bank account manager class structure

--- bank account manager.py
+++ bank account manager.py
@@ -1,15 +1,16 @@
+#program using classes
 class bankaccountmanager:
     def __init__(self,name,starting_bal):
         self.name=name
         self.starting_bal=starting_bal
         self.history=[]
-
+#deposit 
     def deposit(self):
         amount=float(input("enter the amount you want to add"))
         self.starting_bal+=amount
         
         self.history.append(f"amount added {amount}")
-
+#withdraw with if statement if amount withdrwan is more then main balance
     def withdraw(self):
         amount2=float(input("enter the amount to be withdrawn"))
         if amount2>self.starting_bal:
@@ -18,10 +19,10 @@ class bankaccountmanager:
              self.starting_bal-=amount2
              print("amount is taken")
              self.history.append(f"{amount2}")
-
+#to check balance 
     def checkbal(self):
         print(f"the balance in account is {self.starting_bal}")
-
+#to check tranction history
     def view_tranction_history(self):
         print(f"transaction history {self.history}")
 name = input("enter account holder name:")
