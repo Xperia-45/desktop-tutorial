ad3f9685b6c11fd4c17f8c7886d8bcb84298dc5f
Ayush <ayushkatoch2008@gmail.com>
2026-06-30 21:31:25 +0530

    Refactor add_book method to use a dictionary

--- library manager.py
+++ library manager.py
@@ -2,6 +2,8 @@ class library:
        def __init__(self):
               self.books = []
        def add_book(self,title,author):
+              books = {"title":title , "author" : author , checked_out:False}
+              
               
        
        
