d3afd465d95fc915d5bd860308cb7b1b065a0a7a
Ayush <ayushkatoch2008@gmail.com>
2026-07-06 20:46:59 +0530

    Add files via upload

---
+++ to_do_list.py
@@ -0,0 +1,15 @@
+class to_do_list:
+    def __init__(self):
+        self.tasks = [] 
+
+    def add_task(self, task):
+        self.tasks.append({"task": task, "completed": False})
+        print(f'Added task: "{task}".end=')
+    def view_tasks(self):
+        if not self.tasks:
+            print("No tasks in the to-do list.")
+            return
+        print("--- To-Do List ---")
+        for i, task in enumerate(self.tasks, start=1):
+            status = "Completed" if task["completed"] else "Pending"
+            print(f'{i}. {task["task"]} — {status}')
